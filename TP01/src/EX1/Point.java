d81fd997d9d5918065467b87269f9bacd485557a
Eya-Hub <143196960+Eya-Hub@users.noreply.github.com>
2024-09-29 21:55:22 +0100

    Add files via upload

---
+++ TP01/src/EX1/Point.java
@@ -0,0 +1,18 @@
+package EX1;
+
+class Point{
+	private char id;
+	private double abs;
+	
+	public Point (char c,double x) {
+		id = c;
+		abs = x;
+	}
+	public void affiche() {
+		System.out.println("Point identifié par "+id+" d'abscisse "+abs);
+	}
+	public void translate(double dx)
+	{abs +=dx;}
+}
+
+
